9534f90f538eb2cb586d437e904157ca401f297c
Julio Nobrega Netto <inerte@gmail.com>
2023-08-15 17:30:30 -0700

    Display Confirmation items

--- src/Confirmation.jsx
+++ src/Confirmation.jsx
@@ -1,15 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
+import { useAppState } from "./state";
+import { useForm } from "react-hook-form";
+import { navigate } from "wouter/use-location";
 
 const Confirmation = () => {
+  const [state] = useAppState();
+  const { handleSubmit } = useForm({ defaultValues: state });
+
+  const submitData = (data) => {
+    console.info(data);
+  };
+
+  console.log('state', state);
+
   return (
-    <div>
-      <header>
-        <h1>Welcome to Upgrade challenge</h1>
-      </header>
-      <p>
-        To get started, edit <code>src/Confirmation.jsx</code> and save to reload.
-      </p>
-    </div>
+    <>
+      <div className="container">
+        <h1 className="title is-1">Confirmation</h1>
+      </div>
+
+      <ul>
+        <li>First name: {state.firstName}</li>
+        <li>Email: {state.email}</li>
+        <li>Password: {"*".repeat(state.password.length)}</li>
+        <li>Favorite color: {state.favoriteColor}</li>
+        <li>Terms and conditions: {state.termsAndConditions ? "Agreed" : "Not agreed"}</li>
+      </ul>
+
+      <form onSubmit={handleSubmit(submitData)}>
+        <div className="field is-grouped">
+          <div className="control">
+            <button type="button" className="button is-link is-light" onClick={() => navigate("/more-info")}>Back</button>
+          </div>
+          <div className="control">
+            <button className="button is-link">Submit</button>
+          </div>
+        </div>
+      </form>
+    </>
   );
 };
 
