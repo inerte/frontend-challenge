447db18255686e8203a1878459f649e3bf176174
Julio Nobrega Netto <inerte@gmail.com>
2023-08-15 17:08:20 -0700

    Fetch color and add them to the select element

--- src/MoreInfo.jsx
+++ src/MoreInfo.jsx
@@ -1,11 +1,16 @@
+import React from "react";
+import useSWR from "swr";
 import { useForm } from "react-hook-form";
-import React, { Component } from "react";
 import { useAppState } from "./state";
 import { navigate } from "wouter/use-location";
 
 const MoreInfo = () => {
   const [state, setState] = useAppState();
   const { handleSubmit, register } = useForm({ defaultValues: state });
+
+  const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+  const { data, error, isLoading } = useSWR('http://localhost:3001/api/colors', fetcher);
 
   const saveData = (data) => {
     setState({ ...state, ...data });
@@ -22,8 +27,10 @@ const MoreInfo = () => {
           <div className="control">
             <div className="select">
               <select {...register("favorite-color")}>
-                <option>Select dropdown</option>
-                <option>With options</option>
+                <option value="">Select your favorite color</option>
+                {data && data.map((color) => (
+                  <option key={color} value={color}>{color}</option>
+                ))}
               </select>
             </div>
           </div>
